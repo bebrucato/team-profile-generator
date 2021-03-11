c44fdfea38ecf17beaf84fec5c4f839a75cbc6e9
B Brucato <bebrucato@gmail.com>
2021-03-11 09:35:20 -0800

    more template helper and index start

--- src/template-helper.js
+++ src/template-helper.js
@@ -22,8 +22,52 @@ const generateMember = memberArray => {
         }
 
         let (name, role, email, id,) = memberArray[1];
-        card+- `
-        
+        card+= `
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">${name}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+        </div>
+        <ul>
+		    <li>Email: <a href=mailto:${email}>${email}</a></li>
+		    <li>Employee ID: ${id}</li>
+		    <li>${finalProfile}</li>
+        </ul>
+    </div>
         `
     }
+
+    return `
+    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script
+  src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+  crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="style.css"> 
+</head>
+<body>
+<div class="jumbotron">
+<h1 class="text-center">My Team</h1>
+</div> 
+
+<div class="container">
+		<div class="row">
+
+			${card}
+
+		</div>
+	</div>
+</body>
+</html>
+    `
+
 }
+
+generateMember()
